a4dfc68540a256a14f7dbb433cabdd29cad2a1ac
ChapaevAnton <virsus2009@gmail.com>
2021-10-06 11:12:08 +0700

    38.4. External Storage
    * screen cast

--- app/src/main/java/com/w4eret1ckrtb1tch/app38/MainActivity.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app38/MainActivity.kt
@@ -3,6 +3,7 @@ package com.w4eret1ckrtb1tch.app38
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
@@ -53,24 +54,47 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         // TODO: 05.10.2021 Internal Storage
         // TODO: 05.10.2021 context.deleteFile(cacheFileName) и
         //  context.openFileOutput(filename, Context.MODE_PRIVATE) для cache файлов не работает? почему?
-        val file = File("temp.txt")
-        val content = "content"
-        openFileOutput(file.name, Context.MODE_PRIVATE).use { output ->
+        val fileInternal = File("temp_internal.txt")
+        val content = "content_internal"
+        openFileOutput(fileInternal.name, Context.MODE_PRIVATE).use { output ->
             output.write(content.toByteArray())
         }
 
-        openFileInput(file.name).bufferedReader().useLines { lines ->
-            lines.forEach { Log.d("TAG", "lines: $it ") }
+        //v1
+        openFileInput(fileInternal.name).bufferedReader().useLines { lines ->
+            lines.forEach { Log.d("TAG", "useLines: $it ") }
+        }
+        //v2
+        openFileInput(fileInternal.name).bufferedReader().forEachLine { string ->
+            Log.d("TAG", "forEachLine: $string ")
         }
 
-        File.createTempFile("cash_temp", null, cacheDir)
+        File.createTempFile("cache_temp", null, cacheDir)
         val listFiles = cacheDir.listFiles()
         listFiles?.forEach { file ->
-            Log.d("TAG", "cash_add: ${file.name} ")
+            Log.d("TAG", "cache_add: ${file.name} ")
             file.writeBytes(content.toByteArray())
         }
-    }
 
+        // TODO: 06.10.2021 38.4. External Storage
+        val content1 = "content_external"
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            val fileExternal =
+                File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "temp_external.txt")
+            fileExternal.bufferedWriter().use { it.write(content1) }
+
+            fileExternal.bufferedReader().forEachLine { string ->
+                Log.d("TAG", "forEachLine: $string ")
+            }
+
+            val cashFile = File.createTempFile("cache_temp", null, externalCacheDir)
+            cashFile.bufferedWriter().use { it.write(content1) }
+            cashFile.bufferedReader().forEachLine { string ->
+                Log.d("TAG", "forEachLine cache: $string ")
+            }
+        }
+
+    }
 
 
     override fun onDestroy() {
